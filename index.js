a69d40648a12d00002961f488a271bd7c378efe2
rishowrav <rishowrav@gmail.com>
2024-08-15 18:33:56 +0600

    installd mongodb expressjs cors and setup mongodb , expressjs

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+// middle ware
+app.use(cors());
+// middleware end
+
+// mongodb
+
+const uri =
+  "mongodb+srv://elaAdmin:tuWqMoFZ7OHaPrav@cluster0.sp25joa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const allProducts = client.db("elaAdmin").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const products = await allProducts.find().toArray();
+      res.send(products);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongoDB end
+
+app.get("/", (req, res) => {
+  res.send("Hell do World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
